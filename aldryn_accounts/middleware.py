b7c4834a1fc1493565c25cd64005f7c015c8206e
dvanser <ivans.dorohovs@gmail.com>
2025-08-07 12:06:51 +0300

    fix middleware django keyerror

--- aldryn_accounts/middleware.py
+++ aldryn_accounts/middleware.py
@@ -8,33 +8,59 @@ from .utils import geoip
 
 
 class TimezoneMiddleware(object):
+    def __init__(self, get_response=None):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        self.process_request(request)
+        if self.get_response:
+            response = self.get_response(request)
+            return response
+
     def process_request(self, request):
-        tz = request.session.get('django_timezone')
-        if tz:
-            try:
-                timezone.activate(tz)
-            except UnknownTimeZoneError:
-                tz = settings.TIME_ZONE
-                request.session['django_timezone'] = tz
-                timezone.activate(tz)
+        try:
+            tz = request.session.get('django_timezone')
+            if tz:
+                try:
+                    timezone.activate(tz)
+                except UnknownTimeZoneError:
+                    tz = settings.TIME_ZONE
+                    request.session['django_timezone'] = tz
+                    timezone.activate(tz)
+        except (KeyError, AttributeError):
+            # Handle cases where session is not properly initialized
+            pass
 
 
 class GeoIPMiddleware(object):
     """
     Still experimental
     """
+    def __init__(self, get_response=None):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        self.process_request(request)
+        if self.get_response:
+            response = self.get_response(request)
+            return response
+
     def process_request(self, request):
-        ip = request.META.get('HTTP_X_REAL_IP') or request.META.get('REMOTE_ADDR') or None
-        # ip = '67.2.2.25'
-        # ip = '99.27.181.216'  # LA
-        # ip = '92.104.226.167'  # Switzerland (Stefan Home)
-        # ip = '213.189.154.40'  # Switzerland (Divio)
-        data = geoip(ip)
-        if data is not None:
-            request.session['geoip'] = data
-            if not request.session.get('django_timezone') and data.get('time_zone'):
-                request.session['django_timezone'] = data.get('time_zone')
-            if (not (request.session.get('django_location') or request.session.get('django_location_name'))
-                    and data.get('pretty_name') and data.get('latitude') and data.get('longitude') or True):
-                request.session['django_location'] = (data.get('latitude'), data.get('longitude'),)
-                request.session['django_location_name'] = data.get('pretty_name')
+        try:
+            ip = request.META.get('HTTP_X_REAL_IP') or request.META.get('REMOTE_ADDR') or None
+            # ip = '67.2.2.25'
+            # ip = '99.27.181.216'  # LA
+            # ip = '92.104.226.167'  # Switzerland (Stefan Home)
+            # ip = '213.189.154.40'  # Switzerland (Divio)
+            data = geoip(ip)
+            if data is not None:
+                request.session['geoip'] = data
+                if not request.session.get('django_timezone') and data.get('time_zone'):
+                    request.session['django_timezone'] = data.get('time_zone')
+                if (not (request.session.get('django_location') or request.session.get('django_location_name'))
+                        and data.get('pretty_name') and data.get('latitude') and data.get('longitude') or True):
+                    request.session['django_location'] = (data.get('latitude'), data.get('longitude'),)
+                    request.session['django_location_name'] = data.get('pretty_name')
+        except (KeyError, AttributeError):
+            # Handle cases where session is not properly initialized
+            pass
